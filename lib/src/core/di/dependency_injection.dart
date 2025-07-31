ada01fe264228dc9a18b79b7cfdeb703da32d75a
Muhit Mahmud (BS23) <muhit.farhan@brainstation-23.com>
2025-07-31 12:24:10 +0600

    fix: Converted to relative import

--- lib/src/core/di/dependency_injection.dart
+++ lib/src/core/di/dependency_injection.dart
@@ -1,14 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/src/data/repositories/router_repository_impl.dart';
-import 'package:flutter_template/src/domain/use_cases/authentication_use_case.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../data/repositories/authentication_repository_impl.dart';
 import '../../data/repositories/locale_repository_impl.dart';
+import '../../data/repositories/router_repository_impl.dart';
 import '../../data/services/cache/cache_service.dart';
 import '../../data/services/network/endpoints.dart';
 import '../../data/services/network/interceptor/token_manager.dart';
@@ -16,6 +15,7 @@ import '../../data/services/network/rest_client.dart';
 import '../../domain/repositories/locale_repository.dart';
 import '../../domain/repositories/router_repository.dart';
 import '../../presentation/core/router/router.dart';
+import '../../domain/use_cases/authentication_use_case.dart';
 import '../../domain/use_cases/locale_use_case.dart';
 
 part 'dependency_injection.g.dart';
