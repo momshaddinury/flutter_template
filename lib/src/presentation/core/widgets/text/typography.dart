4e685bfb08ad672c450d95cee8e7e13c6c3ae833
Momshad Dinury <dinurymomshad.dev@gmail.com>
2025-12-12 17:17:50 +0600

    feat: Add text rendering properties (maxLines, overflow, softWrap, textDirection, semanticsLabel) to typography widgets.

--- lib/src/presentation/core/widgets/text/typography.dart
+++ lib/src/presentation/core/widgets/text/typography.dart
@@ -3,25 +3,51 @@ import 'package:flutter/material.dart';
 import '../../theme/theme.dart';
 
 abstract class _Typography extends StatelessWidget {
-  const _Typography(this.text, {super.key, this.textAlign});
+  const _Typography(
+    this.text, {
+    super.key,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+    this.softWrap,
+    this.textDirection,
+    this.semanticsLabel,
+  });
 
   final String text;
   final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final bool? softWrap;
+  final TextDirection? textDirection;
+  final String? semanticsLabel;
 
   @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context);
 }
 
 class HeadingLargeText extends _Typography {
-  const HeadingLargeText(super.text, {super.key, super.textAlign});
+  const HeadingLargeText(
+    super.text, {
+    super.key,
+    super.textAlign,
+    super.maxLines,
+    super.overflow,
+    super.softWrap,
+    super.textDirection,
+    super.semanticsLabel,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+      softWrap: softWrap,
+      textDirection: textDirection,
+      semanticsLabel: semanticsLabel,
       style: context.textStyle.headingLarge.copyWith(
         color: context.color.text.primary,
       ),
@@ -30,13 +56,27 @@ class HeadingLargeText extends _Typography {
 }
 
 class HeadingSmallText extends _Typography {
-  const HeadingSmallText(super.text, {super.key, super.textAlign});
+  const HeadingSmallText(
+    super.text, {
+    super.key,
+    super.textAlign,
+    super.maxLines,
+    super.overflow,
+    super.softWrap,
+    super.textDirection,
+    super.semanticsLabel,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+      softWrap: softWrap,
+      textDirection: textDirection,
+      semanticsLabel: semanticsLabel,
       style: context.textStyle.headingSmall.copyWith(
         color: context.color.text.primary,
       ),
@@ -47,32 +87,49 @@ class HeadingSmallText extends _Typography {
 enum _BodyMediumTextVariant { primary, secondary }
 
 class BodyMediumText extends _Typography {
-  const BodyMediumText(super.text, {super.key, super.textAlign})
-    : _variant = _BodyMediumTextVariant.primary;
+  const BodyMediumText(
+    super.text, {
+    super.key,
+    super.textAlign,
+    super.maxLines,
+    super.overflow,
+    super.softWrap,
+    super.textDirection,
+    super.semanticsLabel,
+  }) : _variant = _BodyMediumTextVariant.primary;
 
-  const BodyMediumText.secondary(super.text, {super.key, super.textAlign})
-    : _variant = _BodyMediumTextVariant.secondary;
+  const BodyMediumText.secondary(
+    super.text, {
+    super.key,
+    super.textAlign,
+    super.maxLines,
+    super.overflow,
+    super.softWrap,
+    super.textDirection,
+    super.semanticsLabel,
+  }) : _variant = _BodyMediumTextVariant.secondary;
 
   final _BodyMediumTextVariant _variant;
 
   @override
   Widget build(BuildContext context) {
-    final defaultStyle = context.textStyle.bodyMedium.copyWith(
-      color: context.color.text.primary,
-    );
-
-    final secondaryStyle = context.textStyle.bodyMedium.copyWith(
-      color: context.color.text.secondary,
-      fontWeight: .w500,
-    );
+    final style = switch (_variant) {
+      .primary => context.textStyle.bodyMedium,
+      .secondary => context.textStyle.bodyMedium.copyWith(
+        color: context.color.text.secondary,
+        fontWeight: .w500,
+      ),
+    };
 
     return Text(
       text,
       textAlign: textAlign,
-      style: switch (_variant) {
-        _BodyMediumTextVariant.primary => defaultStyle,
-        _BodyMediumTextVariant.secondary => secondaryStyle,
-      },
+      maxLines: maxLines,
+      overflow: overflow,
+      softWrap: softWrap,
+      textDirection: textDirection,
+      semanticsLabel: semanticsLabel,
+      style: style,
     );
   }
 }
