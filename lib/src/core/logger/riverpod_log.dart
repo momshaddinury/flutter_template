2d435e80f8251188bfa531366357a3254c86d7d2
Mahbub Hasan Talukder <mahbub.talukder@brainstation-23.com>
2026-03-12 15:59:29 +0600

    refactor: migrate RiverpodObserver and core framework to Riverpod v3 API

--- lib/src/core/logger/riverpod_log.dart
+++ lib/src/core/logger/riverpod_log.dart
@@ -2,41 +2,34 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'log.dart';
 
-class RiverpodObserver extends ProviderObserver {
+base class RiverpodObserver extends ProviderObserver {
   @override
-  void didAddProvider(
-    ProviderBase<Object?> provider,
-    Object? value,
-    ProviderContainer container,
-  ) {
-    Log.info('Provider $provider was initialized with $value');
+  void didAddProvider(ProviderObserverContext context, Object? value) {
+    Log.info('Provider ${context.provider} was initialized with $value');
   }
 
   @override
-  void didDisposeProvider(
-    ProviderBase<Object?> provider,
-    ProviderContainer container,
-  ) {
-    Log.warning('Provider $provider was disposed');
+  void didDisposeProvider(ProviderObserverContext context) {
+    Log.warning('Provider ${context.provider} was disposed');
   }
 
   @override
   void didUpdateProvider(
-    ProviderBase<Object?> provider,
+    ProviderObserverContext context,
     Object? previousValue,
     Object? newValue,
-    ProviderContainer container,
   ) {
-    Log.info('Provider $provider updated from $previousValue to $newValue');
+    Log.info(
+      'Provider ${context.provider} updated from $previousValue to $newValue',
+    );
   }
 
   @override
   void providerDidFail(
-    ProviderBase<Object?> provider,
+    ProviderObserverContext context,
     Object error,
     StackTrace stackTrace,
-    ProviderContainer container,
   ) {
-    Log.error('Provider $provider threw $error at $stackTrace');
+    Log.error('Provider ${context.provider} threw $error at $stackTrace');
   }
 }
