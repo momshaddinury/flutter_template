be05ea43ab63e99ba9681712d0e3e06ca66f4658
Md. Shahin Bashar <sbiitju@gmail.com>
2026-03-13 14:33:43 +0600

    refactor: rename `passwordProvider` to `password` in `ConfirmPasswordValidation`

--- lib/src/core/utiliity/validation/confirm_password_validation.dart
+++ lib/src/core/utiliity/validation/confirm_password_validation.dart
@@ -4,14 +4,14 @@ import '../../../core/extensions/app_localization.dart';
 import 'validation.dart';
 
 class ConfirmPasswordValidation extends Validation<String> {
-  ConfirmPasswordValidation({required this.passwordProvider});
+  ConfirmPasswordValidation({required this.password});
 
-  final String Function() passwordProvider;
+  final String Function() password;
 
   @override
   String? validate(BuildContext context, String? value) {
     final confirm = value ?? '';
-    final password = passwordProvider();
+    final password = this.password();
 
     if (confirm != password) {
       return context.locale.passwordMismatchValidation;
